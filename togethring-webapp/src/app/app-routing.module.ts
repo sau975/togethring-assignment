ab77bc4a136139005a0fda53b31503203de51119
Saurav Kumar <saurav97531@gmail.com>
2022-02-04 21:52:31 +0530

    routing

--- togethring-webapp/src/app/app-routing.module.ts
+++ togethring-webapp/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { HomeModule } from './home/home.module';
+import { LoginModule } from './login/login.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- togethring-webapp/src/app/app.module.ts
+++ togethring-webapp/src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { SharedModule } from './shared/shared.module';
+import { RegistrationModule } from './registration/registration.module';
+import { LoginModule } from './login/login.module';
+import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthGuard } from './login/guards/auth.guard';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HomeModule,
+    LoginModule,
+    RegistrationModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
