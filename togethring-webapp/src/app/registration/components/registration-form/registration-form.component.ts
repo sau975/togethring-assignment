eaee7bace2cb205d7d2e27887e597c911f1065b0
Saurav Kumar <saurav97531@gmail.com>
2022-02-05 23:19:36 +0530

    validation added

--- togethring-webapp/src/app/registration/components/registration-form/registration-form.component.ts
+++ togethring-webapp/src/app/registration/components/registration-form/registration-form.component.ts
@@ -32,6 +32,39 @@ export class RegistrationFormComponent implements OnInit {
   register(){
     let data:IRegistration  = Object.assign({}, this.model, this.form.value);
     localStorage.setItem('user', JSON.stringify(data));
+    let user:any = localStorage.getItem('user');
+    let userRegistedDetails = JSON.parse(user);
+    alert("You have successfully Registered. Your User Name is " + userRegistedDetails.userName + " and Password is " + userRegistedDetails.password);
     this.router.navigate(['login']);
   }
+
+  validateEmail(){
+    let email = this.form.get('email')?.value;
+    if(email){
+      var re = /\S+@\S+\.\S+/;
+      return re.test(email);
+    }else{
+      return null;
+    }
+  };
+
+  validatePhone(){
+    let phone = this.form.get('phone')?.value;
+    if(phone){
+      var re = /^[0-9\-\+]{10,10}$/;
+      return re.test(phone);
+    }else{
+      return null;
+    }
+  };
+
+  validatePassword(){
+    let password = this.form.get('password')?.value;
+    if(password){
+      var re = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/;
+      return re.test(password);
+    }else{
+      return null;
+    }
+  };
 }
